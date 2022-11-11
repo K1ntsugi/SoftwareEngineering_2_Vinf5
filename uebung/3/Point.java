4108507cbda3085db08019635aaf0cd5c8cd21d7
Maximilian Groetsch <max-groetsch@gmx.de>
2022-11-11 16:18:36 +0100

    fast fertig

---
+++ uebung/3/Point.java
@@ -0,0 +1,22 @@
+public class Point implements Comparable {
+    private double x, y;
+    private String comment;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point(double x, String comment) {
+        this.x = x;
+        this.comment = comment;
+    }
+
+    public
+
+    @Override public int compareTo(Object arg0) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+}
